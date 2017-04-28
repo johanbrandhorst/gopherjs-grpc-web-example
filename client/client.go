305383804022af33e681e97f00fe7f2d631d6e3b
Johan Brandhorst <johan@cognitivelogic.com>
2017-04-28 23:10:16 +0100

    Add license, generate gopherjs library.

--- client/client.go
+++ client/client.go
@@ -1,7 +1,16 @@
+// Copyright 2017 Johan Brandhorst. All Rights Reserved.
+// See LICENSE for licensing terms.
+
 package main
 
 //go:generate gopherjs build -m client.go -o html/index.js
 //go:generate go-bindata -pkg compiled -nometadata -o compiled/client.go -prefix html ./html
 //go:generate bash -c "rm html/*.js*"
+
+import (
+	"github.com/johanbrandhorst/gopherjs-grpc-web-example/client/proto/library"
+)
+
+var _ = &library.Book{}
 
 func main() {}
--- protogen.sh
+++ protogen.sh
@@ -1,3 +1,6 @@
+// Copyright 2017 Johan Brandhorst. All Rights Reserved.
+// See LICENSE for licensing terms.
+
 #!/usr/bin/env bash
 
-protoc proto/library/book_service.proto --go_out=plugins=grpc:./server/
+protoc proto/library/book_service.proto --go_out=plugins=grpc:./server/ --gopherjs_out=:./client/
